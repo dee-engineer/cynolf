42f3fafeea273b2b6ece83a33df6043ef2ebe228
Chukwu Divine <chukwudivineify@gmail.com>
2024-05-29 10:00:29 -0700

    task one
    update on task

--- app/views.py
+++ app/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
+
+# for Favor, work on user login to user dashboard and admin dashboard
+# email, password, remember_me
+
+
+
+#for Uche, work on user registration
+# first_name, last_name, email, password, confirm_password, address, phone_number, city, state, zip_code, country, date
